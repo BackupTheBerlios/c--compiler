4421e062a5e9352e52448a91be03571ee542e81f
stieli <stieli>
2004-11-16 15:22:54 +0000

    Kommandozeilenargumente

--- vps/src/server.cc
+++ vps/src/server.cc
@@ -7,8 +7,15 @@
 #include <iostream>
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
+	if (argc!=3)
+	{
+		cout<<"Usage: server [clientcount] [steps]\n";
+		exit(-1);
+	}
+	int clients=atoi(argv[1]);
+	int steps=atoi(argv[2]);
 	UDPNetwork* net = new UDPNetwork(7654);
 	LocalBoard* board_a = new LocalBoard(BOARD_WIDTH,BOARD_HEIGHT);
 	LocalBoard* board_b = new LocalBoard(BOARD_WIDTH,BOARD_HEIGHT);
@@ -17,8 +24,8 @@ int main()
 		for ( int y = 0;y < BOARD_HEIGHT;y++ )
 			board_a->setPos(x,y,alive);*/
 	board_a->restoreBoard("test.board");
-	
-	BoardServer* boardserver = new BoardServer((IPNetwork *)net,1,board_a,board_b,100);
-	
+	BoardServer* boardserver = new BoardServer((IPNetwork *)net,clients,board_a,board_b,steps);
 	boardserver->start();
+	
+	board_a->storeBoard("test.result");
 }
